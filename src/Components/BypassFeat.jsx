e905580ff2cc704f7c08a092c39e891a15cf3977
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-14 01:58:15 +0600

    "Updated width styles for BypassFeat and BypassSoln components to include lg:w-[100%] and reordered max-md classes"

--- src/Components/BypassFeat.jsx
+++ src/Components/BypassFeat.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ar1, logo, logo2, arrowright, bypassfeat, bypasssoln, check1, check2, checkcircleicon, checkforai, content, ellipse, flag, framein, frameout, gram1, howitworks, msg, path, plus, rectangle, score, spark1, stars5, upload, vector, web, write1 } from '../Global/Imports.jsx';
 const BypassFeat = () => {
   return (
-    <div className='flex flex-col w-[33%] max-md:ml-0 max-md:w-full'>
+    <div className='flex flex-col w-[33%] max-md:ml-0 lg:w-[100%]'>
       <div className="flex flex-col justify-center items-center">
       <div className="shrink-0 mt-6 max-w-full h-px border border-solid bg-slate-600 border-slate-600 w-[878px]" />
         <div className="mt-32 text-4xl font-medium tracking-tight leading-10 text-center text-slate-50 w-[650px] max-md:mt-10 max-md:max-w-full">
