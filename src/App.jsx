2eb1f8eaf0018e17485e1eeb401522abe9f23d39
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-14 08:24:59 +0600

    Footer updated for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,21 @@ function App() {
 
   return (
     <>
-      <Router>
+      {/* <Router>
         <div className="bg-slate-900">
           <Navbar />
           <main>
             <Routes>
-              <Route path='/' element={<Home/>}></Route>
-              <Route path='/pricing' element={<Pricing/>}></Route>
-              <Route path='/contact' element={<Contact/>}></Route>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/pricing' element={<Pricing />}></Route>
+              <Route path='/contact' element={<Contact />}></Route>
               <Route path='/blog' element={<Blog />}></Route>
               <Route path='/readblog' element={<ReadBlog />}></Route>
             </Routes>
           </main>
           <Footer />
         </div>
-    </Router>
+      </Router> */}
     </>
   )
 }
---
+++ src/Pages/Debugger.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Debugger = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Debugger
